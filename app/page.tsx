215d16f39e2a04fcdce5de14c05653aa38ec4a64
Robert Tavares <rctavares@rogers.com>
2025-06-07 22:42:01 -0300

    fix: update content

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       </div>
 
       <div className="w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p id="hello" className="text-2xl text-center">I am a full-stack developer!</p>
+        <p id="hello" className="text-2xl text-center">I am a web developer!</p>
       </div>
 
       <ButtonOpen />
